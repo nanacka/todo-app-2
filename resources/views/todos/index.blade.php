cdcb25c2121a15541877a1fbe2fd44f4c2347c3f
Mohammed Che <mocherbatji@gmail.com>
2023-10-09 11:55:27 +0100

    Created Views and Seeders

---
+++ resources/views/todos/index.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+ <h2>Todos</h2>
+ <table>
+    <thead>
+        <tr>
+            <th>Title</th>
+            <th>Snippet</th>
+            <th>Created</th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>
+
+        @forelse($todos as $todo)
+            <tr>
+                <td>{{ $todo->title }}</td>
+                <td>{{ $todo->body }}</td>
+                <td>{{ $todo->created_at }}</td>
+                <td><a href="#" >Read more</a></td>
+            </tr>
+
+        @empty
+            <h4>No Todos found!</h4>
+        @endforelse
+        
+    </tbody>
+ </table>
+@endsection
