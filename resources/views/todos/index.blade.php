591f5ce719db6e361c95859b39858adc4712ac6b
nanacka <evemckillop@gmail.com>
2023-10-17 16:45:34 +0100

    added auth and css

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,31 +1,53 @@
-@extends('layouts.app')
+@extends('layouts.myapp')
 
+@section('header')
+<h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+    Todos
+</h2>
+@endsection
 @section('content')
- <h2>Todos</h2>
- <a href="{{ route('todos.create') }}">Create</a>
- <table>
-    <thead>
-        <tr>
-            <th>Title</th>
-            <th>Snippet</th>
-            <th>Created</th>
-            <th></th>
-        </tr>
-    </thead>
-    <tbody>
 
-        @forelse($todos as $todo)
+<a href="{{ route('todos.create') }}">Create</a>
+
+<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-                <td>{{ $todo->title }}</td>
-                <td>{{ $todo->body }}</td>
-                <td>{{ $todo->created_at }}</td>
-                <td><a href="{{ route('todos.show', $todo->id)}}" >Read more</a></td>
+                <th scope="col" class="px-6 py-3">
+                    Title
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Snipper
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Category
+                </th>
             </tr>
-
-        @empty
+        </thead>
+        <tbody>
+            @forelse($todos as $todo)
+            <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {{ $todo->title }}
+                </th>
+                <td class="px-6 py-4">
+                    {{ $todo->body }}
+                </td>
+                <td class="px-6 py-4">
+                    {{ $todo->created_at }}
+                </td>
+                <td class="px-6 py-4">
+                    <td><a href="{{ route('todos.show', $todo->id)}}" >Read more</a></td>
+                </td>
+            </tr>
+            @empty
             <h4>No Todos found!</h4>
         @endforelse
-        
-    </tbody>
- </table>
+
+ 
+        </tbody>
+    </table>
+</div>
+
+
 @endsection
